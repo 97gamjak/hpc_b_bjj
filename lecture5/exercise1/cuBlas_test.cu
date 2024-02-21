79d4069164d822ff39b9170c640b991fef765850
Bernhard Kriesche <krieschebernhard@gmx.at>
2024-02-21 17:19:40 +0100

    Changed lecture5-ex1 to use gpuErrorCheck and modified Makefile accordingly

--- lecture5/exercise1/cuBlas_test.cu
+++ lecture5/exercise1/cuBlas_test.cu
@@ -1,14 +1,10 @@
+#include <cublas_v2.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string>
-#include <cublas_v2.h>
 
-void handle_error(cudaError_t error_id, std::string msg) {
-    if (error_id != cudaSuccess) {
-        printf("Encountered cuda error %d. Msg: \"%s\"\n", error_id, msg.c_str());
-        exit(EXIT_FAILURE);
-    }
-}
+#include <string>
+
+#include "../../cuda_error_check.h"
 
 int main(int argc, char *argv[]) {
     int n = 10000;
@@ -25,7 +21,7 @@ int main(int argc, char *argv[]) {
     // cudaSetDevice(0);
 
     cublasHandle_t handle;
-    cublasStatus_t status = cublasCreate(&handle); 
+    cublasStatus_t status = cublasCreate(&handle);
     if (status != CUBLAS_STATUS_SUCCESS) {
         printf("ERROR: cublasCreate failed. Error code: %d\n", status);
         exit(1);
@@ -34,44 +30,44 @@ int main(int argc, char *argv[]) {
     // - - - - - -
 
     size_t bytes_A = (n * n) * sizeof(double);
-    double* h_A = (double*)malloc(bytes_A); 
-    for(int j=0;j<n;j++)
-        for(int i=0;i<n;i++) 
-            h_A[i + n*j] = i-j;
-    
+    double *h_A = (double *)malloc(bytes_A);
+    for (int j = 0; j < n; j++)
+        for (int i = 0; i < n; i++)
+            h_A[i + n * j] = i - j;
+
     double *d_A;
-    handle_error(cudaMalloc(&d_A, bytes_A), "allocate d_A");
-    handle_error(cudaMemcpy(d_A, h_A, bytes_A, cudaMemcpyHostToDevice), "copy h_A to device");
+    gpuErrorCheck(cudaMalloc(&d_A, bytes_A));
+    gpuErrorCheck(cudaMemcpy(d_A, h_A, bytes_A, cudaMemcpyHostToDevice));
 
     // - - - - - - -
 
     size_t bytes_xy = n * sizeof(double);
 
     double *h_x = (double *)malloc(bytes_xy);
-    for(int j=0; j<n; j++)
+    for (int j = 0; j < n; j++)
         h_x[j] = j;
 
     double *d_x;
-    handle_error(cudaMalloc(&d_x, bytes_xy), "allocate d_x");
-    handle_error(cudaMemcpy(d_x, h_x, bytes_xy, cudaMemcpyHostToDevice), "copy h_x to device");
+    gpuErrorCheck(cudaMalloc(&d_x, bytes_xy));
+    gpuErrorCheck(cudaMemcpy(d_x, h_x, bytes_xy, cudaMemcpyHostToDevice));
 
     double *h_y = (double *)malloc(bytes_xy);
-    for(int j=0; j<n; j++)
+    for (int j = 0; j < n; j++)
         h_y[j] = 0;
 
     double *d_y;
-    handle_error(cudaMalloc(&d_y, bytes_xy), "allocate d_y");
-    handle_error(cudaMemcpy(d_y, h_y, bytes_xy, cudaMemcpyHostToDevice), "copy h_y to device");
+    gpuErrorCheck(cudaMalloc(&d_y, bytes_xy));
+    gpuErrorCheck(cudaMemcpy(d_y, h_y, bytes_xy, cudaMemcpyHostToDevice));
 
     // - - - - - - -
 
     double alpha = 1.0;
-    double beta = 0.0; // if beta=0 then d_y can be uninitialized 
+    double beta = 0.0;  // if beta=0 then d_y can be uninitialized
     cublasDgemv(handle, CUBLAS_OP_N, n, n, &alpha, d_A, n, d_x, 1, &beta, d_y, 1);
 
     // - - - - - - -
 
-    handle_error(cudaMemcpy(h_y, d_y, bytes_xy, cudaMemcpyDeviceToHost), "copy d_y to host");
+    gpuErrorCheck(cudaMemcpy(h_y, d_y, bytes_xy, cudaMemcpyDeviceToHost));
 
     free(h_A);
     free(h_x);
