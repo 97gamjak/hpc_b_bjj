0c75b464d44c6011f1e48e787efb6a26d7e47d19
Josef M. Gallmetzer <gallmetzer.josef@gmail.com>
2024-02-23 20:08:44 +0100

    Refactor deviceQuery.cu to use C++ iostream instead of printf

--- lecture1/exercise1/deviceQuery.cu
+++ lecture1/exercise1/deviceQuery.cu
@@ -1,16 +1,31 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <iostream>
 
-int main()
+using namespace std;
+
+int main(int argc, char *argv[])
 {
     int deviceCount;
     cudaError_t error_id = cudaGetDeviceCount(&deviceCount);
+    if (error_id != cudaSuccess)
+    {
+        printf("cudaGetDeviceCount returned %d\n-> %s\n", (int)error_id, cudaGetErrorString(error_id));
+        printf("Result = FAIL\n");
+        exit(EXIT_FAILURE);
+    }
+
     for (int dev = 0; dev < deviceCount; ++dev)
     {
         cudaSetDevice(dev);
         cudaDeviceProp deviceProp;
         cudaGetDeviceProperties(&deviceProp, dev);
-        printf("\nDevice %d: \"%s\"\n\t> Max gridsize: %d\n\t> Max threadsPerBlock: %d\n", dev, deviceProp.name, deviceProp.maxGridSize, deviceProp.maxThreadsPerBlock);
+        cout << "-------------------------------------------------------------" << endl;
+        cout << "Device " << dev << ": \"" << deviceProp.name << "\"" << endl;
+        cout << "\tCUDA Driver and Runtime version: " << deviceProp.major << "." << deviceProp.minor << endl;
+        cout << "\tTotal amount of global memory: " << deviceProp.totalGlobalMem / 1e9 << " GB" << endl;
+        cout << "\tMaximum number of threads per block: " << deviceProp.maxThreadsPerBlock << endl;
+        cout << "\tUVA support: " << (deviceProp.unifiedAddressing ? "Yes" : "No") << endl;
+        cout << "-------------------------------------------------------------" << endl;
+        
     }
-    return 0;
+    return EXIT_SUCCESS;
 }
