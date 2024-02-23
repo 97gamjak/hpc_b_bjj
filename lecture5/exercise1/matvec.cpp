51d867ff5a10c39fbb9ded2766219f605f8cd43f
Josef M. Gallmetzer <gallmetzer.josef@gmail.com>
2024-02-23 15:23:01 +0100

    Refactor matvec.cpp to include error checking and measure elapsed time

--- lecture5/exercise1/matvec.cpp
+++ lecture5/exercise1/matvec.cpp
@@ -2,8 +2,14 @@
 #include <cublas_v2.h>
 #include <cuda_runtime.h>
 #include <iostream>
+#include "../../cuda_error_check.h"
 
 using namespace std;
+
+#define CUBLAS_ASSERT(x) if ((x) != CUBLAS_STATUS_SUCCESS) { \
+    cout << "Error at " << __FILE__ << ":" << __LINE__ << endl; \
+    exit(1); \
+}
 
 void initialize_matrix(double *A, int n);
 void initialize_vector(double *x, int n);
@@ -16,13 +22,12 @@ int main(int argc, char **argv)
 {
     int n = read_args(argc, argv);
 
+    cudaEvent_t start, stop;
+    cudaEventCreate(&start);
+    cudaEventCreate(&stop);
+
     cublasHandle_t handle;
-    cublasStatus_t status = cublasCreate(&handle);
-    if (status != CUBLAS_STATUS_SUCCESS)
-    {
-        cout << "ERROR: cublasCreate failed. Error code: " << status << endl;
-        exit(1);
-    }
+    CUBLAS_ASSERT(cublasCreate(&handle));
 
     size_t size_A = n * n * sizeof(double);
     size_t size_x = n * sizeof(double);
@@ -34,15 +39,15 @@ int main(int argc, char **argv)
     initialize_vector(h_x, n);
 
     double *d_A;
-    cudaMalloc(&d_A, size_A);
-    cudaMemcpy(d_A, h_A, size_A, cudaMemcpyHostToDevice);
+    gpuErrorCheck(cudaMalloc(&d_A, size_A));
+    gpuErrorCheck(cudaMemcpy(d_A, h_A, size_A, cudaMemcpyHostToDevice));
 
     double *d_x;
-    cudaMalloc(&d_x, size_x);
-    cudaMemcpy(d_x, h_x, size_x, cudaMemcpyHostToDevice);
+    gpuErrorCheck(cudaMalloc(&d_x, size_x));
+    gpuErrorCheck(cudaMemcpy(d_x, h_x, size_x, cudaMemcpyHostToDevice));
 
     double *d_y;
-    cudaMalloc(&d_y, size_x);
+    gpuErrorCheck(cudaMalloc(&d_y, size_x));
 
     double alpha = 1.0;
     double beta = 0.0; // if beta=0 then d_y can be uninitialized
@@ -52,27 +57,38 @@ int main(int argc, char **argv)
     // op = CUBLAS_OP_C (conjugate transpose of A).
     
     // Start the timer
-    double a = clock();   
+    cudaEventRecord(start, 0);  
 
     // Usually lda=n, incx=1, and incy=1
-    cublasDgemv(handle, op, n, n, &alpha, d_A, n, d_x, 1, &beta, d_y, 1);
+    CUBLAS_ASSERT(cublasDgemv(handle, op, n, n, &alpha, d_A, n, d_x, 1, &beta, d_y, 1));
     cudaDeviceSynchronize();
 
     // Stop the timer
-    double b = clock();
+    cudaEventRecord(stop, 0);
 
-    cudaMemcpy(h_y, d_y, size_x, cudaMemcpyDeviceToHost);
+    gpuErrorCheck(cudaMemcpy(h_y, d_y, size_x, cudaMemcpyDeviceToHost));
 
     verify_result(h_y, n);
 
     // Destroy the handle
-    cublasDestroy(handle);
+    CUBLAS_ASSERT(cublasDestroy(handle));
 
     // Free the memory
-    cudaFree(d_A); cudaFree(d_x); cudaFree(d_y);
-    free(h_A); free(h_x); free(h_y);
+    gpuErrorCheck(cudaFree(d_A)); 
+    gpuErrorCheck(cudaFree(d_x)); 
+    gpuErrorCheck(cudaFree(d_y));
+    free(h_A); 
+    free(h_x); 
+    free(h_y);
 
-    cout << n << " in " << (b - a) / CLOCKS_PER_SEC * 1e6 << " µs" << endl;
+    // Calculate the time
+    cudaEventSynchronize(stop);
+    float elapsedTime;
+    cudaEventElapsedTime(&elapsedTime, start, stop);
+    cout << "Elapsed time: " << elapsedTime << " ms" << endl;
+
+    cudaEventDestroy(start);
+    cudaEventDestroy(stop);
 
     return 0;
 }
