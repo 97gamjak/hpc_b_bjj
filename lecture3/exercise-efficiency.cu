208b7dff80ddc91f7793c74fe1cc6a1f6135b83a
Josef M. Gallmetzer <gallmetzer.josef@gmail.com>
2024-02-24 20:15:38 +0100

    Further improvement - initialize vector

--- lecture3/exercise-efficiency.cu
+++ lecture3/exercise-efficiency.cu
@@ -1,8 +1,10 @@
 #include <iostream>
 #include <cmath>
 #include <fstream>
-using namespace std;
 
+#include "../cuda_error_check.h"
+
+using namespace std;
 
 __global__
 void k_matvecmul(long n, double* in, double* out, double* mat) {
@@ -12,9 +14,16 @@ void k_matvecmul(long n, double* in, double* out, double* mat) {
         out[i] = mat[0]*in[i] + mat[1]*in[i+1] + mat[2]*in[i-1];
 }
 
+__global__
+void initialize_vector(long n, double* in) {
+    long i = threadIdx.x + blockDim.x*blockIdx.x;
+    if(i<n)
+        in[i] = sin(double(i) / double(n - 1) * M_PI);
+}
+
 int main() {
     long n = 1e6;
-    long time_steps = 100;
+    long time_steps = 1000;
     double *h_in, *d_mat;
     double *d_in, *d_out;
 
@@ -27,27 +36,26 @@ int main() {
     cudaMalloc(&d_in, sizeof(double) * n);
     cudaMalloc(&d_out, sizeof(double) * n);
 
+    // Set up the kernel launch parameters.
+    int threads_per_block = 256;
+    int blocks_per_grid = n/threads_per_block + 1;
+
     cudaEventRecord(start, 0);
 
-    // intialize vector and copy to GPU
-    for (long i = 0; i < n; i++)
-        h_in[i] = sin(double(i) / double(n - 1) * M_PI);
-
-    cudaMemcpy(d_in, h_in, sizeof(double) * n, cudaMemcpyHostToDevice);
+    // initialize vector and copy to GPU
+    initialize_vector<<<blocks_per_grid, threads_per_block>>>(n, d_in);
 
     // initialize matrix and copy to GPU
     double matrix_row[3] = {1.0 - 0.25 * 2.0, 0.25, 0.25};
     cudaMalloc(&d_mat, sizeof(double) * 3);
     cudaMemcpy(d_mat, matrix_row, sizeof(double) * 3, cudaMemcpyHostToDevice);
 
-    int threads_per_block = 64;
-    int blocks_per_grid = n/threads_per_block + 1;
-
     // repeated matrix-vector multiplication (i.e. time integration)
     for(long k=0;k<time_steps;k++) {
         k_matvecmul<<<blocks_per_grid, threads_per_block>>>(n, d_in, d_out, d_mat);
-
-        cudaMemcpy(d_in, d_out, sizeof(double) * n, cudaMemcpyDeviceToDevice);
+        double* temp = d_in;
+        d_in = d_out;
+        d_out = temp;
     }
 
     cudaEventRecord(stop, 0);
